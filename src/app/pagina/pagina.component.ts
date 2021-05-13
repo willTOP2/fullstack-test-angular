2038bcb9c0e38839da91b2f83448d7119d21d963
Cristiano Fromagio <cristianofromagio@gmail.com>
2021-05-13 09:06:12 -0300

    example setup

--- src/app/pagina/pagina.component.ts
+++ src/app/pagina/pagina.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { catchError } from 'rxjs/operators';
+
+import { ApiService } from '../services/api.service';
+
 @Component({
   selector: 'app-pagina',
   templateUrl: './pagina.component.html',
@@ -7,9 +11,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PaginaComponent implements OnInit {
 
-  constructor() { }
+  public apiGreeting = '';
+
+  constructor(
+    private apiService: ApiService
+  ) { }
 
   ngOnInit(): void {
+    this.apiService.getHello().pipe(
+      catchError((err) => {
+        this.apiGreeting = 'Falha na comunicação com o servidor.';
+        return [];
+      })
+    ).subscribe((response) => {
+      if (response) {
+        this.apiGreeting = response.mensagem;
+      }
+    });
   }
 
 }
---
+++ src/app/services/api.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  public getHello(): Observable<any> {
+    return this.http.get(environment.apiHost + '/hello');
+  }
+
+}
